8d9137412af641043064fc547942ebd48772998a
unknown <chisomdarlynton@gmail.com>
2026-08-19 03:13:03 +0100

    correction of database

--- bookstore-django/books/config/settings.py
+++ bookstore-django/books/config/settings.py
@@ -66,11 +66,10 @@ WSGI_APPLICATION = 'books.config.wsgi.application'
 
 import dj_database_url
 
+DATABASE_URL = config('DATABASE_URL', default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
-        conn_max_age=600,
-    )
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
