1c3907bd06750d68e69cc0e8b50d3642c3e06cef
unknown <chisomdarlynton@gmail.com>
2026-08-19 02:46:47 +0100

    fix ALLOWED_HOSTS cast

--- bookstore-django/books/config/settings.py
+++ bookstore-django/books/config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = config('SECRET_KEY', ...)
 DEBUG = config('DEBUG', ...)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', ...)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 INSTALLED_APPS = [...]
 MIDDLEWARE = [...]
